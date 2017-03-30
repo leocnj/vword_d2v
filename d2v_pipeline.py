46adb1988d315ddcc731b8004924aec1742137f4
Chen, Lei <LChen@ETS.ORG>
2017-03-30 11:00:10 -0400

    now d2v can be added into a pipeline
    ppl = Pipeline([('d2v', d2v_transformer(50, 10))])
        d2v = ppl.transform(txts)

--- d2v_pipeline.py
+++ d2v_pipeline.py
@@ -18,6 +18,7 @@ import pprint
 
 # sklearn
 from sklearn.base import TransformerMixin
+from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
 
 # Update train_d2v.py to be able to
@@ -53,47 +54,45 @@ class LabeledLineSentence(object):
 
 
 class d2v_transformer(TransformerMixin):
-    """Takes in dataframe, extracts road name column, outputs average word length"""
+    """Takes in a list of text, run doc2vec on it, outputs vectors"""
 
-    def __init__(self, d2v_dim):
+    def __init__(self, d2v_dim, context_win):
         self.d2v_dim = d2v_dim
-
-    def d2v_txts(self, X):
-        sentences = LabeledLineSentence(X)
-        model = Doc2Vec(sentences, min_count=1, window=10, size=self.d2v_dim, sample=1e-4, negative=5, workers=7)
-        # model.build_vocab(sentences.to_array())
-        for epoch in range(50):
-            logger.info('Epoch %d' % epoch)
-            model.train(sentences.sentences_perm())
-
-        feat_array = numpy.zeros((len(X), self.d2v_dim))
-        for i in range(len(X)):
-            prefix = 'SENT_' + str(i)
-            feat_array[i] = model.docvecs[prefix].tolist()
-        return feat_array
+        self.context_win = context_win
 
     def transform(self, X, y=None):
         """The workhorse of this feature extractor"""
-        return self.d2v_txts(X)
+        return get_d2v(X, self.d2v_dim, self.context_win)
 
     def fit(self, X, y=None):
         """Returns `self` unless something different happens in train and test"""
         return self
 
 
-
-with open('vword/trans.txt', 'r') as fin:
-    txts = fin.readlines()
+def get_d2v(txts, d2v_dim, context_win):
     sents = LabeledLineSentence(txts)
-    model = Doc2Vec(sents, min_count=1, window=10, size=50, sample=1e-4, negative=5, workers=7)
+    model = Doc2Vec(sents, min_count=1, window=context_win, size=d2v_dim, sample=1e-4, negative=5, workers=7)
     for epoch in range(50):
         logger.info('Epoch %d' % epoch)
         model.train(sents)
 
-    feat_array = numpy.zeros((len(txts), 50))
+    feat_array = numpy.zeros((len(txts), d2v_dim))
     for i in range(len(txts)):
         prefix = 'SENT_' + str(i)
         feat_array[i] = model.docvecs[prefix].tolist()
+    return feat_array
 
-print(feat_array)
+
+with open('vword/trans.txt', 'r') as fin:
+    txts = fin.readlines()
+    # d2v = get_d2v(txts, d2v_dim=50, context_win=10)
+
+    # use transformer directly
+    # d2v_tf = d2v_transformer(50, 10)
+    # d2v = d2v_tf.transform(txts)
+
+    # using transformer in a pipeline.
+    ppl = Pipeline([('d2v', d2v_transformer(50, 10))])
+    d2v = ppl.transform(txts)
+    print(d2v)
 
